30ed8fe952c0a23d1cb866994b8d0caa862a6410
SamuelLima0610 <samvlima10@gmail.com>
2024-04-09 10:20:09 -0300

    Captura das informações de todas as franquias atuais de basquete NBA

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 from header import HEADER
 from bs4 import BeautifulSoup, Comment
-
+import time as tempo
 
 def pegar_linhas_tabela(text, id_tabela):
     soup = BeautifulSoup(text)
@@ -30,40 +30,47 @@ def pegar_linhas_tabela(text, id_tabela):
 
 def pegar_tabela_escondida(response, id):
     soup = BeautifulSoup(response.text)
-    placeholders = soup.find_all(string=lambda text: isinstance(text, Comment))
-    tabelas_ids = ["team_and_opponent",]
-    for placeholder in placeholders:
+    comentarios_na_pagina = soup.find_all(string=lambda text: isinstance(text, Comment))
+    for comentario in comentarios_na_pagina:
         try:
-            soup = BeautifulSoup(placeholder, "html.parser")
-            ids = [table['id'] for table in soup.find_all('table')]
-            print(ids)
+            soup = BeautifulSoup(comentario, "html.parser")
             tabela = soup.find(attrs={"id": id})
             if tabela is not None:
-                return pegar_linhas_tabela(placeholder, id)
+                return pegar_linhas_tabela(comentario, id)
         except:
             pass
 
-def get_html(url, path):
-    session = requests.session()
-    session.headers = HEADER
-    session.headers["path"] = path
-    response = session.get(url + path)
+def pegar_html(url, path):
+    sessao = requests.session()
+    sessao.headers = HEADER
+    sessao.headers["path"] = path
+    response = sessao.get(url + path)
+    sessao.close()
     return response
 
 def main():
     URL = "https://www.basketball-reference.com"
-    response = get_html(URL, "/teams/")
+    response = pegar_html(URL, "/teams/")
+    tempo.sleep(2)
     infos_times = pegar_linhas_tabela(response.text, 'teams_active')
-    endpoint = infos_times[0]['Franchise']['href']
-    response = get_html(URL, endpoint)
-    infos_time = pegar_linhas_tabela(response.text, endpoint.split('/')[-2])
-    endpoint = infos_time[0]['Season']['href']
-    response = get_html(URL, endpoint)
-    per_game = pegar_linhas_tabela(response.text,'per_game')
-    totals = pegar_linhas_tabela(response.text, 'totals')
-    team_and_opponent = pegar_tabela_escondida(response, "team_and_opponent")
-    team_misc = pegar_tabela_escondida(response, "team_misc")
-    injuries = pegar_tabela_escondida(response, "injuries")
-    print(totals)
+    infos = []
+    for info_time in infos_times:
+        try:
+            endpoint = info_time['Franchise']['href']
+            response = pegar_html(URL, endpoint)
+            time = endpoint.split('/')[-2]
+            tempo.sleep(2)
+            infos_time = pegar_linhas_tabela(response.text, time)
+            endpoint = infos_time[0]['Season']['href']
+            response = pegar_html(URL, endpoint)
+            tempo.sleep(2)
+            per_game = pegar_linhas_tabela(response.text,'per_game')
+            totals = pegar_linhas_tabela(response.text, 'totals')
+            team_and_opponent = pegar_tabela_escondida(response, "team_and_opponent")
+            team_misc = pegar_tabela_escondida(response, "team_misc")
+            dict_time = {'time': time, 'totals': totals, 'per_game': per_game, 'team_misc': team_misc, 'team_and_opponent': team_and_opponent}
+            infos.append(dict_time)
+        except:
+            pass
 
 main()
